92be2447f33303d1b28c816b423180e71c21008f
Michael Rowe <michaelrowe01@gmail.com>
2022-02-05 15:53:05 -0500

    again

---
+++ swiftSerd/serd_config.h
@@ -0,0 +1,11 @@
+/* WARNING! All changes made to this file will be lost! */
+
+#ifndef W_SWIFT_SERD_CONFIG_H_WAF
+#define W_SWIFT_SERD_CONFIG_H_WAF
+
+#define SWIFT_SERD_VERSION "0.5.0"
+/* #undef HAVE_FILENO */
+/* #undef HAVE_POSIX_FADVISE */
+/* #undef HAVE_POSIX_MEMALIGN */
+
+#endif /* W_SWIFT_SERD_CONFIG_H_WAF */
